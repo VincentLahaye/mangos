d8354e096114b1ae98a5f147b2df89bf138e1152
Vincent Lahaye <flousedid@gmail.com>
2011-08-24 12:46:47 +0200

    test

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -607,7 +607,8 @@ void AreaAura::Update(uint32 diff)
                         for(GroupReference *itr = pGroup->GetFirstMember(); itr != NULL; itr = itr->next())
                         {
                             Player* Target = itr->getSource();
-                            if (Target && Target->isAlive() && Target->GetSubGroup()==subgroup && caster->IsFriendlyTo(Target))
+                            //if (Target && Target->isAlive() && Target->GetSubGroup()==subgroup && caster->IsFriendlyTo(Target))
+							if (Target && Target->isAlive() && Target->GetSubGroup()==subgroup)
                             {
                                 if (caster->IsWithinDistInMap(Target, m_radius))
                                     targets.push_back(Target);
